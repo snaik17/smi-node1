58c4e5f31ecfa290658e4cd460e430fcbfd9d2fe
snaik17 <smith.naik7@gmail.com>
2019-09-27 12:20:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody smith!");
+  res.send("Hello from Appsody smith naik!");
 });
  
 module.exports.app = app;
